c9b480eebfc1ac487c5b9885318492d09fe398a6
Hatsumi07 <64712742+Hatsumi07@users.noreply.github.com>
2022-05-14 19:50:48 -0500

    Update card.py

--- card.py
+++ card.py
@@ -1,19 +1,19 @@
 #creates a class for a card. Card object has a card number (self.card) and the points for each right guess
 class Card:
         def __init__(self):
-                self.card = random.randint(1, 13)
+                self.card_number = random.randint(1, 13)
                 self.points = 0
 #Method check_guess takes two parameters "guess" and "previous_card". The last is what it will be compared to.
         def check_guess(self, guess, previous_card):
                 if guess == "h":
-                        if previous_card < self.card:
+                        if previous_card < self.card_number:
                                 return True
-                        elif previous_card > self.card:
+                        elif previous_card > self.card_number:
                                 return False
                 elif guess == "l":
-                        if previous_card < self.card:
+                        if previous_card < self.card_number:
                                 return False
-                        elif previous_card > self.card:
+                        elif previous_card > self.card_number:
                                 return True
 #Method get_points gets the points made according to the success of the guess.
         def get_points(self, guess, previous_card):
