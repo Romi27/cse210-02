902c85c2d26af617d47b9b1ecb40a0cb78ef634b
Hatsumi07 <64712742+Hatsumi07@users.noreply.github.com>
2022-05-25 08:55:49 -0500

    Update player.py

--- player.py
+++ player.py
@@ -3,23 +3,21 @@ from card import Card
 #"score" which is the sum of the points made for each guess and "is_playing" which determines if the game can 
 #continue according to the score.
 class Captain:
- 
-
 
     def __init__(self):
         self.cards = [Card()]
         self.score = 300
-        self.is_playing = self.check_life()
-          
+        self.is_playing = True
+    
     def check_life(self):#Checks if the score is enough to continue playing
         if self.score > 0:
-            return True
+            self.is_playing = True
         elif self.score <= 0:
-            return False
+            self.is_playing = False
 
     def get_inputs(self):#Gets the input.
-        card_guess = input("higher or lower? [h/l] ")
-        return card_guess
+        card_guess = str(input("Higher or lower? [h/l] "))
+        return card_guess.lower()
 
 #Updates the attirbutes. Creates a new Card object to be the "next card" and adds it to the cards list.
 #Gets the guess and updates the score. 
@@ -33,25 +31,25 @@ class Captain:
 #Outputs the cards and the score.
     def do_outputs(self):
         self.do_updates()  
-        print(f"Next card is: {self.cards[1].card}")
+        print(f"Next card was: {self.cards[1].card}")
         print(f"Your score is: {self.score}\n")
-        self.cards.pop(0)
+        play_again = input('Do you want to keep playing (y/n)? ')
+        if  play_again.lower() == "y":
+            self.is_playing = True
+            self.cards.pop(0)
+        else:
+            self.is_playing = False
     
     def play(self):
-        while self.is_playing == True:
+        while self.is_playing:
             current_card = self.cards[0]
-            print(f"The card is: {current_card.card}\n")
+            print(f"The card is: {current_card.card_number}\n")
             self.do_outputs()
             if self.is_playing == False:
+                print("Good game!")
                 break
         
-    
 def main():
-    game=Captain()    
-    game.play()
-
-
-if __name__ == "__main__":
-    main()
-
-
+    new_game = Captain()
+    new_game.play()
+main()
